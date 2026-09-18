c2992cf16b7be3a4320add8ae5f3f6b8106aa81c
emmanuelnyamorah-glitch <emmanuelnyamorah@gmail.com>
2026-09-18 10:41:21 +0300

    Changes to code

--- boolean_2.py
+++ boolean_2.py
@@ -1,4 +1,5 @@
 age=27
+
 is_member=False
 has_day_pass=True
 if (is member or has day pass)
---
+++ gymmembership.py
@@ -0,0 +1,6 @@
+is_member=True 
+has_day_pass=False 
+   if(is member or has day pass):
+    print("Access granted")
+    else:
+        print("Access denied")
